6ee7af42750890f6bfb57316ecd82998b3cf4aaa
Ariful <arif98741@gmail.com>
2022-04-29 03:50:18 +0600

    removed machine updte code

--- routes/web.php
+++ routes/web.php
@@ -2,15 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::group(['prefix' => 'admin','as'=>'admin.'], function () {
-    Route::get('/dashboard', 'Admin\AdminController@dashboard')->name('dashboard');
-
-    Route::resource('/blog', 'Admin\BlogController');
-});
 
 Route::get('/', function(){
-    return "hello this laravel app ";
+    phpinfo();
 });
 
-//Auth::routes();
+
+Route::get('hello', function(){
+    return 'hello';
+});
+
+
 
